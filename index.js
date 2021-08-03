ce8d48fe0334c9d47322e0393ea42f52cd8936cb
Ben Zhang <bzhang0@cs.washington.edu>
2021-08-02 23:43:58 -0700

    v1.4 - time script to be implemented

---
+++ index.js
@@ -0,0 +1,55 @@
+"use strict";
+
+(function() {
+
+  const TIME_URL = "https://www.timeapi.io/api/Time/current/zone";
+  const TIME_ZONE = "America/Los_Angeles";
+
+  // https://www.timeapi.io/api/Time/current/zone?timeZone=America/Los_Angeles
+
+
+
+  window.addEventListener("load", init);
+
+  function init() {
+    requestTime();
+  }
+
+
+  /**
+   * Returns the element that has the ID attribute with the specified value.
+   * @param {string} idName - element ID
+   * @returns {object} DOM object associated with id.
+   */
+  function id(idName) {
+    return document.getElementById(idName);
+  }
+
+  /**
+   * Returns the first element that matches the given CSS selector.
+   * @param {string} selector - CSS query selector.
+   * @returns {object} The first DOM object matching the query.
+   */
+  function qs(selector) {
+    return document.querySelector(selector);
+  }
+
+  /**
+   * Returns the array of elements that match the given CSS selector.
+   * @param {string} selector - CSS query selector
+   * @returns {object[]} array of DOM objects matching the query.
+   */
+  function qsa(selector) {
+    return document.querySelectorAll(selector);
+  }
+
+  /**
+   * Returns a new element with the given tag name.
+   * @param {string} tagName - HTML tag name for new DOM element.
+   * @returns {object} New DOM object for given HTML tag.
+   */
+  function gen(tagName) {
+    return document.createElement(tagName);
+  }
+
+})();
